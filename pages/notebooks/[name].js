c92676bb0446008281fcd3ecb5c9eb7feb0bf0b4
juniferd <jyk@jyk>
2020-10-14 12:25:15 -0700

    feat: clicking on image zooms in

--- pages/notebooks/[name].js
+++ pages/notebooks/[name].js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import fs from  'fs';
 import path from 'path';
 import gsap from 'gsap';
@@ -6,20 +6,37 @@ import styled from 'styled-components';
 import { useRouter } from 'next/router';
 import Link from '../../components/Link';
 import { NOTEBOOKS_DIR, getNotebookDirs} from '../../helpers';
-import { lgSpacing } from '../../components/constants';
+import { smSpacing, lgSpacing } from '../../components/constants';
 import { mediaQuery } from '../../components/mediaQueries';
 
-const Image = styled.div`
+const SelectedImage = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  background: rgba(0, 0, 0, .85);
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > img {
+    background: white;
+    max-height: calc(100vh - ${lgSpacing});
+    height: auto;
+    width: auto;
+    max-width: calc(100% - ${lgSpacing});
+  }
+`;
+
+const ImageContainer = styled.div`
   background: white;
   width: calc(100vw / 3);
   max-width: 300px;
   box-shadow: 0 0 12px rgba(204, 143, 0, .1);
   position: relative;
   height: auto;
-  margin-bottom: ${lgSpacing};
-  &:not(:last-child) {
-    margin-right: ${lgSpacing};
-  }
+  margin: 0 ${smSpacing} ${lgSpacing};
   > img {
     width: 100%;
   }
@@ -31,7 +48,7 @@ const Image = styled.div`
     }
   `)}
 `;
-const ImageContainer = styled.div`
+const Section = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
@@ -42,6 +59,7 @@ const Notebook = ({ content }) => {
   // each notebook has images
   const router = useRouter();
   const { name } = router.query;
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
     // TODO maybe use refs instead of global css class
@@ -56,22 +74,30 @@ const Notebook = ({ content }) => {
         each: 0.25,
       },
     });
-  });
+  }, []);
 
   return (
     <>
       <h1>{name}</h1>
       <p><Link href="/notebooks">&larr;</Link></p>
-      <ImageContainer>
+      {!!selectedImage ? (
+        <SelectedImage
+          onClick={(_) => setSelectedImage(null)}
+        >
+          <img src={selectedImage} />
+        </SelectedImage>
+      ) : null}
+      <Section>
         {content[name].map(img => (
-          <Image
+          <ImageContainer
             className="notebookImage"
             key={img.filename}
+            onClick={(_) => setSelectedImage(img.content)}
           >
             <img src={img.content} />
-          </Image>
+          </ImageContainer>
         ))}
-      </ImageContainer>
+      </Section>
     </>
   )
 }
@@ -82,10 +108,16 @@ export async function getStaticProps() {
     const imgDir = path.join(NOTEBOOKS_DIR, notebook)
     const imgPaths = fs.readdirSync(imgDir);
 
-    const res = imgPaths.map(img => ({
-      filename: img,
-      content: `/images/notebooks/${notebook}/${img}`,
-    }))
+    const IMG_EXTS = ['.png', '.bmp', '.gif', '.jpeg', '.jpg', '.tiff', '.svg'];
+    const res = imgPaths
+      .filter(file => IMG_EXTS.includes(path.extname(file).toLowerCase()))
+      .map(img => {
+        return {
+          filename: img,
+          content: `/images/notebooks/${notebook}/${img}`,
+        }
+      });
+
     return [notebook, res];
   });
 
